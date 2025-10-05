c77ce81027cdac5cb45c8418760f5da246e0af25
varunheycoach <varunheycoach@gmail.com>
2025-10-05 09:44:51 +0530

    created jukebox modal and added ACE-Step repo

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,32 @@
 import modal
 
-app = modal.App('jukebox')
+app = modal.App("jukebox")
 
 image = (
     modal.Image.debian_slim()
     .apt_install('git')
     .pip_install_from_requirements('requirements.txt')
-    .run_commands(["git clone https://github.com/ace-step/ACE-Step.git /tmp/ACE-Step", "cd /tmp/ACE-Step && pip install ."])
+    .run_commands(['git clone https://github.com/ace-step/ACE-Step.git /tmp/ACE-Step', 'cd /tmp/ACE-Step && pip install .'])
     .env({"HF_HOME": "/.cache/huggingface"})
+    .add_local_python_source("prompts")
 )
+
+model_volume = modal.Volume.from_name("ace-step-models", create_if_missing=True)
+hf_volume = modal.Volume.from_name("qwen-hf-cache", create_if_missing=True)
+
+music_gen_secret = modal.Secret.from_name("music-gen-secret")
+
+@app.cls(
+    image=image,
+    gpu="L405",
+    volumes={"/models": model_volume, "/.cache/huggingface": hf_volume}
+    secrets=[music_gen_secret]
+    scaledown_window=15
+)
+class MusicGenServer:
+    @modal.enter()
+    def load_model(self):
+        pass
+@app.local_entrypoint()
+def main():
+    pass
